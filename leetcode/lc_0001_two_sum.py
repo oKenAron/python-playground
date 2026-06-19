61edbfd3fe1c0df62099b5a818455dee4d996299
oKenAron <wu.naqin.uz@tut.jp>
2026-06-19 18:49:56 +0900

    perf(1): 引入字典将查找优化为 O(n)

--- leetcode/lc_0001_two_sum.py
+++ leetcode/lc_0001_two_sum.py
@@ -1,19 +1,18 @@
-from typing import List
-
-
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length = len(nums)
-        for i in range(length - 1):
-            for j in range(i + 1, length):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        raise ValueError("输入异常")
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # 创建字典，键为value，值为index
+        seen: dict[int, int] = {}
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [i, seen[target - nums[i]]]
+            elif nums[i] not in seen:
+                seen[nums[i]] = i
+        raise ValueError("Error")
 
 
 if __name__ == "__main__":
     solution = Solution()
-    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
-    assert solution.twoSum([3, 2, 4], 6) == [1, 2]
-    assert solution.twoSum([3, 3], 6) == [0, 1]
+    assert solution.twoSum([2, 7, 11, 15], 9) in ([0, 1], [1, 0])
+    assert solution.twoSum([3, 2, 4], 6) in ([1, 2], [2, 1])
+    assert solution.twoSum([3, 3], 6) in ([0, 1], [1, 0])
     print("All test cases passed!")
