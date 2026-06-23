57bf9f1fd8994b376247c7560fd7bd8b8bbfaeb0
oKenAron <wu.naqin.uz@tut.jp>
2026-06-23 13:00:30 +0900

    refactor(1108): replace split/join with str.replace

--- leetcode/problems/lc_1108.py
+++ leetcode/problems/lc_1108.py
@@ -1,4 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        address_split = address.split(".")
-        return "[.]".join(address_split)
+        return address.replace(".", "[.]")
