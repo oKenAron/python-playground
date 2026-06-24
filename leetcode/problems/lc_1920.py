cd0706d5857a128018ba1b2434604d16071918bc
oKenAron <wu.naqin.uz@tut.jp>
2026-06-25 08:33:40 +0900

    feat(1920): follow-up O(1) 空间原地编码实现

--- leetcode/problems/lc_1920.py
+++ leetcode/problems/lc_1920.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[num] for num in nums]
+        n = len(nums)
+        for i in range(n):
+            nums[i] += (nums[nums[i] % n] % n) * n
+        return [num // n for num in nums]
 
 
 if __name__ == "__main__":
@@ -11,3 +14,4 @@ if __name__ == "__main__":
 
     assert solution.buildArray([0, 2, 1, 5, 3, 4]) == [0, 1, 2, 4, 5, 3]
     assert solution.buildArray([5, 0, 1, 2, 3, 4]) == [4, 5, 0, 1, 2, 3]
+    print("All test cases passed!")
